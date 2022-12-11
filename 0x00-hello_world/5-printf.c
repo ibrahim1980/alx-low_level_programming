85a8db77ebaa280476d52b6f6639d974b0c9e0f2
root <ibrahimmwarimbo@gmail.com>
2022-12-11 03:50:37 -0800

    program that prints exactly with proper grammar, but the
    outcome is a piece of art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,4 @@
 #!/bin/bash
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
-printf("with proper grammar, but the outcome is a piece of art,")
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
