b6d545325acdf29615786212457936b78bcdefd7
root <ibrahimmwarimbo@gmail.com>
2022-12-13 03:20:17 -0800

    Printf Command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,10 @@
  *  main-Entry Point                                                                                                                   
  *  Return always 0 where success     
  *
+ * Description:  A column of asterisks on the left side
  */                                                                                                                               
 int main(void)                                                                                                                          
 {                                                                                                                                       
 	 printf("with proper grammar but the outcome is a piece of art");
-	  return (0);                                                                                                                   
+	 return (0);                                                                                                                   
 } 
