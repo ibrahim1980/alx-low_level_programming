35ab23e2c13b82450f0a565861ce7bbeac5fe144
root <ibrahimmwarimbo@gmail.com>
2022-12-13 02:31:10 -0800

    with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
-printf("with proper grammar, but the outcome is a piece of art,");
-return 0;
+#include<stdio.h>
+/**
+ *main-Entry Point
+ *
+ *Retun always 0 where success
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+  return (0);
+}
+
