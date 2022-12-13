57187a053bf727e90ea88b1205472b2435476451
root <ibrahimmwarimbo@gmail.com>
2022-12-13 02:48:51 -0800

    Printf Command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-/**
- *main-Entry Point
- *
- *Retun always 0 where success
- */
-int main(void)
-{
- printf("with proper grammar, but the outcome is a piece of art,");
- return (0);
-}
+#include<stdio.h>                                                                                                                       
 
+/**                                                                                                                                     
+ *  main-Entry Point                                                                                                                   
+ *  Return always 0 where success     
+ *
+ */                                                                                                                               
+int main(void)                                                                                                                          
+{                                                                                                                                       
+	 printf("with proper grammar but the outcome is a piece of art");
+	  return (0);                                                                                                                   
+} 
