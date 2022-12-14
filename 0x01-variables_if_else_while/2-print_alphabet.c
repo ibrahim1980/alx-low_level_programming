8c8482190226b71cb5d8a75fbfef7c3c6a33fe2a
root <ibrahimmwarimbo@gmail.com>
2022-12-13 17:17:47 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return:ALways 0 (succcess)
+ */
+
+int main(void)
+{
+	char c;
+
+	c='a';
+	while
+		(c<='z'){
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
