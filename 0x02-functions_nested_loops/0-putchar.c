56f03345ef7ea9e93b4fd75793f3a5624e934903
root <ibrahimmwarimbo@gmail.com>
2022-12-14 09:53:53 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main is the start point
+ *
+ *always return 0 (success)
+ *
+ * Add _putchar
+*/
+int main(void)
+{
+_putchar(_);
+_putchar(p);
+_putchar(u);
+_putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+_putchar(r);
+_putchar('\n');
+return (0);
+}
