6dc773f4fd97eb35bda2be4234a934722ce2f9b7
root <ibrahimmwarimbo@gmail.com>
2022-12-14 10:18:36 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print all alphabet with lower case
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+*/
+void print_alphabet(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+       _putchar(letter);
+
+return (0);
+}
