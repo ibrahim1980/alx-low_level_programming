b87689ee1f93c51b11ff52528102fb3098308518
root <ibrahimmwarimbo@gmail.com>
2022-12-16 10:21:03 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /* main.h*/
+#endif /* main_h*/
