d999b065779095099c2275d736f3b2763035ccd6
root <ibrahimmwarimbo@gmail.com>
2022-12-15 17:54:36 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *   print_sign - Prints the sign of a number
  *    @n: The number to be checked
@@ -22,5 +21,5 @@ int print_sign(int n)
 		{
 			_putchar(48);
 			return (0);
-			}
+		}
 }
