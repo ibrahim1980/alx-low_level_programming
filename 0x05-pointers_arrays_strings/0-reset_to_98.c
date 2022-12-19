516def1b92c4135eb3e7d39811bbae283bde282b
root <ibrahimmwarimbo@gmail.com>
2022-12-19 02:32:24 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - check the code 
+ *
+ *Return: Always 0.
+ *
+*/
+
+void reset_to_98(int *n)
+{
+	int n;
+	n = 98;
+	_putchar("n=%d\n", n);
+	return (0);
+}
+
+
+
