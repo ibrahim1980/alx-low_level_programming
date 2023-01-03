1c5cea939d6a8206284cc634e0e55574c39a522f
root <ibrahimmwarimbo@gmail.com>
2023-01-03 02:38:40 -0800

    0x07. C - Even more pointers, arrays and strings

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#include<stdio.h>                                                                                                                       
-
-/**                                                                                                                                     
- *  main-Entry Point                                                                                                                   
- *  Return always 0 where success     
+#include<stdio.h>
+/**
+ *  main-Entry Point
  *
- * Description:  A column of asterisks on the left side
- */                                                                                                                               
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	 printf("with proper grammar but the outcome is a piece of art");
-	 return (0);                                                                                                                   
-} 
+ *  Return always 0 where success
+*/
+int main(void)
+{
+	printf("with proper grammar but the outcome is a piece of art");
+	return (0);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 /**
- *
- * *
- *
- * */
+ *  main - Entry point
+ *  
+ *  Return: Always 0 (Success)
+*/
 int main(void)
 {
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
 	printf("Size of a char: %lu byte(s)\n",sizeof(char));
 	printf("Size of a int: %lu byte(s)\n",sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n",sizeof(long int));
