6e26e5546aee6fffd33d0deac2dc57e27c5bca93
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-16 22:00:22 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,19 +4,19 @@
 #include <unistd.h>
 /**
  * main - main function
+ *
  * Return: 1
  */
 int main(void)
 {
 	char *string;
 	char str1[] = "and that piece of art is useful\"";
-	char str2[] = "- Dora Korpar, 2015-10-19";
+	char str2[] = "- Dora Korpar, 2015-10-2019";
 
 	string = malloc(strlen(str1) + strlen(str2) + 1);
 	strcpy(string, str1);
-	strcpy(string, str2); 
-	strncat(str1, str2,25);
-	printf(str1);
+	strcpy(string, str2);
+	write(2, string, sizeof(string));
 
 return (1);
 }
