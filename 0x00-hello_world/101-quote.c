38f5a5541972bc13032f719770a4ee8afa1b8bd4
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-18 09:27:40 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main - main for puts
- * Return: 1
- */
+* main - main for puts 
+* Return: 1
+*/
 int main(void)
-{ 
+{
 	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-2019\n", 59);
         return (1);
 }
