c80db051170596d4bf73dcb5ee18672cb50144a8
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-18 10:10:47 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,17 +4,15 @@
  *
  * Return:ALways 0 (succcess)
  */
-
 int main(void)
 {
 	char c;
-
 	c='a';
 	while
 		(c<='z'){
-			putchar(c);
-			c++;
+		putchar(c);
+		c++;
 		}
 	putchar('\n');
 	return (0);
-}
+	}
