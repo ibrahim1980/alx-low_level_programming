18aa1b1be26903d68de2aa4e204ef4e03afdd715
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-20 11:45:33 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
+ * main - Entry point
  *
- *
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,16 +12,16 @@ int main(void)
 
 	c = 'a';
 	d = 0;
-	while 
-		(d < 10){
-			putchar(d + '0');
-			d++;
-		}
-	while
-		(c <= 'f'){
-			putchar(c);
-			c++;
-		}
+	while (d < 10)
+	{
+		putchar(d + '0');
+		d++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
