acb470ee83b311f4aa4719c3bb3f841b21227d4e
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-21 12:11:47 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,23 +2,20 @@
 
 /**
  *
- *   * _abs - Computes the absolute value of an integer.
+ *   _abs - Computes the absolute value of an integer.
  *
- *     * @c: The number to be computed.
+ *   @c: The number to be computed.
+ * 
+ *   Return: Absolute value of number or zero
  *
- *       *
- *
- *         * Return: Absolute value of number or zero
- *
- *           */
-
+ */
 int _abs(int c)
 {
 	if (c < 0)
-		{
-			int abs_val;
-			abs_val = c * -1;
-			return (abs_val);
-			}
+	{
+		int abs_val;
+		abs_val = c * -1;
+		return (abs_val);
+	}
 	return (c);
 }
