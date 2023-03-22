dd37a4f06ef2ef031c73828cebf0d1998645b69d
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-22 11:00:34 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,11 +6,10 @@
 int main(void)
 {
 	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
 	
+	printf("Infinite loop incoming :(\n");
+	
+	i = 0;
 	/**
 	*while (i < 10)
 	*{
@@ -18,7 +17,6 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
 
