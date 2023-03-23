c565409c9ee2c3b341ff485dd4b4f4acdf4d8ba7
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-23 14:13:54 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,36 @@
-#include "main.h"
+#include "main.h" 
 
 /**
- * print_most_numbers - a function that prints the numbers, from 0 to 9
- *
- * Return: 0-9, excluding 2 and 4, followed by new line
+ * print_number - a function that prints an integer.
+ * @n: An input integer
+ * Return: Nothing
  */
 void print_number(int n)
-{ 
-
-	for (n = 0; n <= 9; n++)
+{
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
 	{
-		if ((n != 2) || (n != 4))
-			continue;
-		else
-			_putchar(n + '0');
+		_putchar('-');
+		print_integer(n * -1);
 	}
-	_putchar('\n');
+	else
+		print_integer(n);
 }
+
+/**
+ * print_integer - A function to priting n
+ * @m: an input unsigned integer
+ * Return: Nothing
+ */
+void print_integer(int m)
+{
+	int i = 1000000000;
+
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
+}
+
