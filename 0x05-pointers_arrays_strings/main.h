419466467038b1454569b57badbf0f50869cef4d
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-28 12:42:16 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdlib.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,6 +7,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
-
-#endif
+char *_strcpy(char *dest, char *src); 
