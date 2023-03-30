8e647275b142530b4a3d3594c2791632aa7fea8d
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-03-30 21:35:57 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,13 +2,21 @@
 #include <stdio.h>
 
 /**
+ * write your line of code here...
  *
- *
- *
- *
+ * - you are not allowed to use a
+ * - you are not allowed to code anything else than this line of code
 */
 
 void print_number(int n)
 {
+  int len = 0, i;
 
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n'); 
 }
