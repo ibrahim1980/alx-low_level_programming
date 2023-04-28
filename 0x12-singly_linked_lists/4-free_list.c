e3933d06e866a4b2533b1bb756c2b68c874f1139
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-04-28 11:29:34 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,7 +9,7 @@
  * free_listint - Frees a listint_t list.
  * @head: A pointer to the head of the listint_t list to be freed.
  */
-void free_listint(listint_t *head)
+void free_list(list_t *head)
 {
 	listint_t *tmp;
 
