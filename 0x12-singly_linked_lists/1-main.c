a8e2d3c52d84f53c922a43fb667431afd1debb57
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-04-28 11:23:09 +0300

    Update 1-main.c

--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -1,32 +1,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "../lists.h"
+#include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	listint_t *head;
-	listint_t *new;
-	listint_t hello = {8, NULL};
-	size_t n;
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
 
-	head = &hello;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->n = 9;
-	new->next = head;
-	head = new;
-	n = listint_len(head);
-	printf("-> %lu elements\n", n);
-	free(new);
-	return (0);
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = list_len(head);
+    printf("-> %lu elements\n", n);
+    free(new->str);
+    free(new);
+    return (0);
 }
