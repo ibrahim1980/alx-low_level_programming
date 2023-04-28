6372bf327120431a3baabdb0b4b5dc8738a7d48a
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-04-28 11:28:38 +0300

    Update 4-main.c

--- 0x12-singly_linked_lists/4-main.c
+++ 0x12-singly_linked_lists/4-main.c
@@ -1,28 +1,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "../lists.h"
+#include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	listint_t *head;
+    list_t *head;
 
-	head = NULL;
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 2);
-	add_nodeint_end(&head, 3);
-	add_nodeint_end(&head, 4);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
-	print_listint(head);
-	free_listint(head);
-	head = NULL;
-	return (0);
+    head = NULL;
+    add_node_end(&head, "Bob");
+    add_node_end(&head, "&");
+    add_node_end(&head, "Kris");
+    add_node_end(&head, "love");
+    add_node_end(&head, "asm");
+    print_list(head);
+    free_list(head);
+    head = NULL;
+    return (0);
 }
