cee872cf923924bc7cc1a822d2c5503478ba7c3a
ibrahim1980 <ibrahimmwarimbo@yahoo.com>
2023-05-05 13:27:57 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,27 +1,22 @@
-/*
- * File: 5-flip_bits.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
+#include "main.h"
 /**
- * flip_bits - Counts the number of bits needed to be
- *             flipped to get from one number to another.
- * @n: The number.
- * @m: The number to flip n to.
- *
- * Return: The necessary number of bits to flip to get from n to m.
+ * flip_bits - flip to get from one number to another
+ * @n: input
+ * @m: bits
+ * Return: 0
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m, bits = 0;
+	unsigned int count = 0;
 
-	while (xor > 0)
+	while (n != 0 || m != 0)
 	{
-		bits += (xor & 1);
-		xor >>= 1;
+		if ((n & 1) != (m & 1))
+			count++;
+		m = m >> 1;
+		n = n >> 1;
 	}
 
-	return (bits);
+	return (count);
+
 }
